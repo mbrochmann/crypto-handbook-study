19776332b1789bec2922a7011c7379371d4063d9
Michelle <mbrochmann@gmail.com>
2020-12-02 06:04:47 +0000

    implemented is_bt_smooth recursively (2)

--- is_pt_smooth.py
+++ is_pt_smooth.py
@@ -1,6 +1,7 @@
 # is_pt_smooth.py>
 
 import sympy;
+import numpy;
 
 # test by trial division whether b is pt smooth
 # if pt smooth, return the order of the factors
@@ -11,27 +12,51 @@ def is_pt_smooth(b,t):
     pt=sympy.prime(t)
     print("{}'th prime: {}".format(t,pt))
     print("===")
-    get_factor_degree(b,pt)
-
+    exponent,quotient=get_factor_degree_recursive(b,pt)
+    if ( quotient == 1 ):
+        return True
+    elif t == 1:
+        return False
+    else:
+        return is_pt_smooth(quotient,t-1)
+    
 
 # how many times is n1 divided evenly by n2?
 def get_factor_degree(n1,n2):
+    if ( n2 == 0 ):
+        return -1,n1
+    if ( n2 > n1 ):
+        return 0,n1
     exponent=0
-    dividend=n1
+    quotient=n1
     # "loop-and-a-half"
     while True:
-        tempdividend=dividend/n2
-        remainder=n1%n2
-        print("{}/{}: {}".format(dividend,n2,tempdividend))
-        print("{}%{}: {}".format(dividend,n2,remainder))
+        tempquotient=quotient/n2
+        remainder=quotient%n2
+        print("{}/{}: {}".format(quotient,n2,tempquotient))
+        print("{}%{}: {}".format(quotient,n2,remainder))
         print("===")
         if ( remainder != 0 ):
             break
         else:
             exponent+=1
-            dividend=tempdividend
-            if ( dividend == 1 ):
+            quotient=tempquotient
+            if ( quotient == 1 ):
                 break
-    print("{}={}^{}*{}".format(n1,n2,exponent,dividend))
+    print("{}={}^{}*{}".format(n1,n2,exponent,quotient))
+    return exponent,quotient
 
-
+def get_factor_degree_recursive(n1,n2):
+    quotient=n1/n2
+    remainder=n1%n2
+    print("{}/{}: {}".format(n1,n2,quotient))
+    print("{}%{}: {}".format(n1,n2,remainder))
+    print("===")
+    if ( remainder != 0 ):
+        return 0,n1;
+    elif ( quotient == 1 ):
+        return 1,1;
+    else:
+        return numpy.add( (1,0), get_factor_degree_recursive(quotient,n2) )
+    
+    
